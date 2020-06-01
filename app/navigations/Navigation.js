ed1fbb2623180cbca4bdbc1364b11cbba79d7aa9
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-01 14:46:18 -0400

    Seguimos mejorando NavigationContainer

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import TopRestaurants from "../screens/TopRestaurants";
-import Search from "../screens/Search";
 
 import Restaurants from "../screens/Restaurants";
 import Favorites from "../screens/Favorites";
+import TopRestaurants from "../screens/TopRestaurants";
+import Search from "../screens/Search";
+import Account from "../screens/Account";
 
 const Tab = createBottomTabNavigator();
 
@@ -28,6 +29,16 @@ export default function Navigation() {
           name="top-restaurants"
           component={TopRestaurants}
           options={{ title: "Top 5" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={Search}
+          options={{ title: "Search" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={Account}
+          options={{ title: "Account" }}
         />
       </Tab.Navigator>
     </NavigationContainer>
