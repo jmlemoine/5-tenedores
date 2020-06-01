bafedf635dc3368e8b4f9fb7e77f114bca6ed87e
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-01 10:53:56 -0400

    Creando la app

---
+++ app/screens/TopRestaurants.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TopRestaurants() {
+  return (
+    <View>
+      <Text>TopRestaurants</Text>
+    </View>
+  );
+}
+
+/*
+yarn add @react-navigation/native@~5.1.5
+
+expo install react-native-gesture-handler@~1.6.0 
+expo install react-native-reanimated@~1.7.0 
+expo install react-native-screens@~2.2.0 
+expo install react-native-safe-area-context@~0.7.3 
+expo install @react-native-community/masked-view@~0.1.6
+
+yarn add @react-navigation/stack@~5.2.10
+yarn add @react-navigation/bottom-tabs@~5.2.6
+
+*/
