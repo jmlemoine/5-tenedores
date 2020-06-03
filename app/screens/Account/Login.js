83a2eb322fa16b9832d59093b64a0a7e3804b19f
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-03 11:48:10 -0400

    Register

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,10 +1,65 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { Divider } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Logun() {
+export default function Login() {
   return (
-    <View>
+    <ScrollView>
+      <Image
+        source={require("../../../assets/img/original.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <View style={styles.viewContainer}>
+        <Text>Login</Text>
+        <CreateAccount />
+      </View>
+      <Divider style={styles.divider} />
+      <Text>Social Login</Text>
+    </ScrollView>
+    /*<View>
       <Text>Login</Text>
-    </View>
+    </View>*/
   );
 }
+
+function CreateAccount() {
+  const navigation = useNavigation();
+  //  console.log(navigation);
+  return (
+    <Text style={styles.textRegister}>
+      Aún no tiene cuenta? {""}
+      <Text
+        style={styles.btnRegister}
+        onPress={() => navigation.navigate("register")}
+      >
+        Sign Up
+      </Text>
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewContainer: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+  textRegister: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  btnRegister: {
+    color: "#00a680",
+  },
+  divider: {
+    backgroundColor: "#00a680",
+    margin: 40,
+  },
+});
