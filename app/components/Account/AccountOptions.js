fa1fe4cb45f47528885827f06ce1c188a3a6e152
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-09 21:31:38 -0400

    Personalizando la lista de opciones

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -5,29 +5,75 @@ import { map } from "lodash";
 
 export default function AccountOptions(props) {
   const { userInfo, toastRef } = props;
-  const menuOptions = generateOptions();
-  console.log(menuOptions);
+
+  //console.log(menuOptions);
+
+  const selectComponent = (key) => {
+    console.log("Click!");
+    console.log(key);
+  };
+
+  const menuOptions = generateOptions(selectComponent);
+
   return (
     <View>
       {map(menuOptions, (menu, index) => (
-        <ListItem key={index} title={menu.title} />
+        <ListItem
+          key={index}
+          title={menu.title}
+          leftIcon={{
+            type: menu.iconType,
+            name: menu.iconNameLeft,
+            color: menu.iconColorLeft,
+          }}
+          rightIcon={{
+            type: menu.iconType,
+            name: menu.iconNameRight,
+            color: menu.iconColorRight,
+          }}
+          containerStyle={styles.menuItem}
+          onPress={menu.onPress}
+        />
       ))}
     </View>
   );
 }
 
-function generateOptions() {
+function generateOptions(selectComponent) {
   return [
     {
       title: "Cambiar Nombre y Apellidos",
+      iconType: "material-community",
+      iconNameLeft: "account-circle",
+      iconColorLeft: "#ccc",
+      iconNameRight: "chevron-right",
+      iconColorRight: "#ccc",
+      onPress: () => selectComponent("displayName"),
     },
     {
       title: "Cambiar Correo Electrónico",
+      iconType: "material-community",
+      iconNameLeft: "at",
+      iconColorLeft: "#ccc",
+      iconNameRight: "chevron-right",
+      iconColorRight: "#ccc",
+      onPress: () => selectComponent("email"),
     },
     {
       title: "Cambiar Contraseña",
+      iconType: "material-community",
+      iconNameLeft: "lock-reset",
+      iconColorLeft: "#ccc",
+      iconNameRight: "chevron-right",
+      iconColorRight: "#ccc",
+      onPress: () => selectComponent("password"),
     },
   ];
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  menuItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3",
+  },
+});
