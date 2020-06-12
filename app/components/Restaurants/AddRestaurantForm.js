38e957edd0cb0aa18df4f52b614293a1a334c56b
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-12 10:31:07 -0400

    Guardando las images en la consola

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -8,19 +8,22 @@ import {
   Dimensions,
 } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
-import AddRestaurant from "../../screens/Restaurants/AddRestaurant";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 export default function AddRestaurantForm(props) {
   const { toastRef, setIsLoading, navigation } = props;
   const [restaurantName, setRestaurantName] = useState("");
   const [restaurantAddress, setRestaurantAddress] = useState("");
   const [restaurantDescription, setRestaurantDescription] = useState("");
+  const [imagesSelected, setImagesSelected] = useState([]);
 
   const addRestaurant = () => {
     console.log("OK");
-    console.log("Restaurant Name: " + restaurantName);
+    /*console.log("Restaurant Name: " + restaurantName);
     console.log("Restaurant Address: " + restaurantAddress);
-    console.log("Restaurant Description: " + restaurantDescription);
+    console.log("Restaurant Description: " + restaurantDescription);*/
+    console.log(imagesSelected);
   };
 
   return (
@@ -30,7 +33,11 @@ export default function AddRestaurantForm(props) {
         setRestaurantAddress={setRestaurantAddress}
         setRestaurantDescription={setRestaurantDescription}
       />
-      <UploadImage />
+      <UploadImage
+        toastRef={toastRef}
+        imagesSelected={imagesSelected}
+        setImagesSelected={setImagesSelected}
+      />
       <Button
         title="Crear Restaurante"
         onPress={addRestaurant}
@@ -68,9 +75,33 @@ function FormAdd(props) {
   );
 }
 
-function UploadImage() {
-  const imageSelect = () => {
-    console.log("Imágenes...");
+function UploadImage(props) {
+  const { toastRef, imagesSelected, setImagesSelected } = props;
+  const imageSelect = async () => {
+    const resultPermissions = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    if (resultPermissions === "denied") {
+      toastRef.current.show(
+        "Es necesario aceptar los permisos de la galeria, si los has rechazado tienes que ir ha ajustes y activarlos manualmente.",
+        3000
+      );
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+      console.log(result);
+      if (result.cancelled) {
+        toastRef.current.show(
+          "Has cerrado la galería sin elegir una imagen",
+          2000
+        );
+      } else {
+        setImagesSelected([...imagesSelected, result.uri]);
+      }
+    }
   };
   return (
     <View style={styles.viewImage}>
