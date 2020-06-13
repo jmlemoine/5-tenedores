317a614568384cd3d8ba7a26a9217ab02239621c
jmlemoine <jeanmelvinlp27@gmail.com>
2020-06-13 04:19:45 -0400

    Mostrando la imagen principal del restaurante

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -12,6 +12,8 @@ import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import { map, size, filter } from "lodash";
 
+const widthScreen = Dimensions.get("window").width;
+
 export default function AddRestaurantForm(props) {
   const { toastRef, setIsLoading, navigation } = props;
   const [restaurantName, setRestaurantName] = useState("");
@@ -29,6 +31,7 @@ export default function AddRestaurantForm(props) {
 
   return (
     <ScrollView style={styles.scrollView}>
+      <ImageRestaurant imagenRestaurant={imagesSelected[0]} />
       <FormAdd
         setRestaurantName={setRestaurantName}
         setRestaurantAddress={setRestaurantAddress}
@@ -45,6 +48,22 @@ export default function AddRestaurantForm(props) {
         buttonStyle={styles.btnAddRestaurant}
       />
     </ScrollView>
+  );
+}
+
+function ImageRestaurant(props) {
+  const { imagenRestaurant } = props;
+  return (
+    <View style={styles.viewPhoto}>
+      <Image
+        source={
+          imagenRestaurant
+            ? { uri: imagenRestaurant }
+            : require("../../../assets/img/no-image.png")
+        }
+        style={{ width: widthScreen, height: 200 }}
+      />
+    </View>
   );
 }
 
@@ -188,5 +207,10 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     marginRight: 10,
+  },
+  viewPhoto: {
+    alignItems: "center",
+    height: 200,
+    marginBottom: 20,
   },
 });
